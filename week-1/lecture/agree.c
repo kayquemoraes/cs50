9f1d375b14df0b974d575a1b3e017f356be1edac
Kayque Moraes <kaymoraesdias@gmail.com>
2023-02-21 18:06:19 -0300

    Conditionals, Logical operators

---
+++ week-1/lecture/agree.c
@@ -0,0 +1,20 @@
+// Conditionals, Logical operators
+
+#include "cs50.h"
+#include <stdio.h>
+
+int main(void)
+{
+    // Prompt user to agree
+    char c = get_char("Do you agree? y/n\n");
+
+    // Check whether agreed
+    if (c == 'y' || c == 'Y')
+    {
+        printf("Agreed.\n");
+    }
+    else if (c == 'n' || c == 'N')
+    {
+        printf("Not agreed.\n");
+    }
+}
