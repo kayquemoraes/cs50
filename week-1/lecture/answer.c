456d39e37c70ec956898925c6dc923d4dba8215b
Kayque Moraes <kaymoraesdias@gmail.com>
2023-02-21 17:48:36 -0300

    Variables, get_string and printf with incorrect placeholder

---
+++ week-1/lecture/answer.c
@@ -0,0 +1,10 @@
+//Variables, get_string and printf with incorrect placeholder
+
+#include "cs50.h"
+#include <stdio.h>
+
+int main(void)
+{
+    string answer = get_string("What's your name?\n");
+    printf("hello, answer\n");
+}
