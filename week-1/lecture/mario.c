4366993c0876eb999b2a2391f8f08c592969f539
Kayque Moraes <kaymoraesdias@gmail.com>
2023-02-21 20:49:53 -0300

    Loops, Prints an n-by-n grid of bricks, re-prompting user for positive integer

--- week-1/lecture/mario.c
+++ week-1/lecture/mario.c
@@ -1,12 +1,19 @@
-// Prints an n-by-n grid of bricks with nested loops
+// Loops, Prints an n-by-n grid of bricks, re-prompting user for positive integer
 
 #include "cs50.h"
 #include <stdio.h>
 
 int main(void)
 {
-    const int n = get_int("Size: ");
+    // Get size of grid
+    int n;
+    do
+    {
+        n = get_int("Size: ");
+    }  
+    while (n < 1);
 
+    // Print grid of bricks
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < n; j++)
