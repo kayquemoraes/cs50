199c12df172d2f5ae833d877c176db0a34cd5fd8
Kayque Moraes <kaymoraesdias@gmail.com>
2023-02-21 18:48:00 -0300

    Loops, Better design with while

--- week-1/lecture/meow.c
+++ week-1/lecture/meow.c
@@ -1,10 +1,13 @@
-// Loops, Opportunity for better design
+// Loops, Better design with while
 
 #include <stdio.h>
 
 int main(void)
 {
-    printf("meow\n");
-    printf("meow\n");
-    printf("meow\n");
+    int i = 0;
+    while (i < 3)
+    {
+        printf("meow\n");
+        i++;
+    }
 }
