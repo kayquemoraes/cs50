5fccf999c3c5dc522efe81254a110cf7e933a5a9
Kayque Moraes <kaymoraesdias@gmail.com>
2023-03-28 20:46:55 -0300

    Arrays

--- week-2/lecture/score.c
+++ week-2/lecture/score.c
@@ -1,13 +1,16 @@
-// Averages three (hardcoded) numbers
+// Averages three (hardcoded) numbers using an array
+
+#include <cs50.h>
 #include <stdio.h>
 
 int main(void)
 {
     // Scores
-    int score1 = 72;
-    int score2 = 73;
-    int score3 = 33;
+    int scores[3];
+    scores[0] = 72;
+    scores[1] = 73;
+    scores[2] = 33;
 
     // Print average
-    printf("Average: %f\n", (score1 + score2 + score3) / 3.0);
+    printf("Average: %f\n", (scores[0] + scores[1] + scores[2]) / 3.0);
 }
