0758cb13aef995e4da2b37574011c38cf9b85306
Kayque Moraes <kaymoraesdias@gmail.com>
2023-03-28 21:51:54 -0300

    Array of strings, array of another array

--- week-2/lecture/hi.c
+++ week-2/lecture/hi.c
@@ -1,10 +1,15 @@
-// Prints string's ASCII codes
+// Array of strings
 
-#include "cs50.h"
+#include <cs50.h>
 #include <stdio.h>
 
 int main(void)
 {
-    string s = "HI!";
-    printf("%i %i %i\n", s[0], s[1], s[2]);
+    string words[2];
+
+    words[0] = "HI!";
+    words[1] = "BYE!";
+
+    printf("%s\n", words[0]);
+    printf("%s\n", words[1]);
 }
