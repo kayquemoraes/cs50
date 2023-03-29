7b599c6d2d972083f15d3b0057591b55dcbf1bf2
Kayque Moraes <kaymoraesdias@gmail.com>
2023-03-28 21:48:25 -0300

    Strings, how a string can be used as a array

--- week-2/lecture/hi.c
+++ week-2/lecture/hi.c
@@ -1,12 +1,10 @@
-// Prints chars' ASCII codes
+// Prints string's ASCII codes
 
+#include "cs50.h"
 #include <stdio.h>
 
 int main(void)
 {
-    char c1 = 'H';
-    char c2 = 'I';
-    char c3 = '!';
-
-    printf("%i %i %i\n", c1, c2, c3);
+    string s = "HI!";
+    printf("%i %i %i\n", s[0], s[1], s[2]);
 }
