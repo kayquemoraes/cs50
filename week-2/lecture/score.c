40f591509fc69ee2983d7105d2667c46391ffb07
Kayque Moraes <kaymoraesdias@gmail.com>
2023-03-28 21:02:17 -0300

    Arrays passed between functions

--- week-2/lecture/score.c
+++ week-2/lecture/score.c
@@ -1,16 +1,34 @@
-// Averages three (hardcoded) numbers using an array
+// Averages three numbers using an array, a constant, and a helper function
 
-#include <cs50.h>
+#include "cs50.h"
 #include <stdio.h>
+
+// Constant
+const int N = 3;
+
+// Prototype
+float average(int length, int array[]);
 
 int main(void)
 {
-    // Scores
-    int scores[3];
-    scores[0] = 72;
-    scores[1] = 73;
-    scores[2] = 33;
+    // Get scores
+    int scores[N];
+    for (int i = 0; i < N; i++)
+    {
+        scores[i] = get_int("Score: ");
+    }
 
     // Print average
-    printf("Average: %f\n", (scores[0] + scores[1] + scores[2]) / 3.0);
+    printf("Average: %f\n", average(N, scores));
+}
+
+float average(int length, int array[])
+{
+    // Calculate average
+    int sum = 0;
+    for (int i = 0; i < length; i++)
+    {
+        sum += array[i];
+    }
+    return sum / (float) length;
 }
