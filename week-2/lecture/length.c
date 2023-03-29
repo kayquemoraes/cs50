55a11e440da85d859cfd5ebf74d6f2f9195a0cf2
Kayque Moraes <kaymoraesdias@gmail.com>
2023-03-28 22:10:47 -0300

    Length of a string using a function

--- week-2/lecture/length.c
+++ week-2/lecture/length.c
@@ -1,19 +1,28 @@
-// Determines the length of a string
+// Determines the length of a string using a function
 
 #include "cs50.h"
 #include <stdio.h>
+
+int string_length(string s);
 
 int main(void)
 {
     // Prompt for user's name
     string name = get_string("Name: ");
 
+    int length = string_length(name);
+    printf("%i", length);
+
+    
+}
+
+int string_length(string s)
+{
     // Count number of characters up until '\0' (aka NUL)
     int i = 0;
-    while (name[i] != '\0')
+    while (s[i] != '\0')
     {
         i++;
     }
-
-    printf("%i\n", i);
+    return i;
 }
